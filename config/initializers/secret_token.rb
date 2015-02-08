ba3c9f62eafaa838aa0c5652356c170a31c99db0
Bearuser <bearuser@codelion.net>
2015-02-08 10:55:07 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Comment::Application.config.secret_key_base = '476469e176b62c45ce92adc0d662dfef76ce904f25fa243818ff532b0d66981a3486c565e7d1e831733554362b48d109c321576bd7eda1a805b4e3db57ab810e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Comment::Application.config.session_store :cookie_store, key: '_comment_session'
